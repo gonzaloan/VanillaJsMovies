9f47ef5f1f046ba47480e396a54987f440d4903c
Gonzalo Munoz <gonzaloan.munoz@gmail.com>
2019-01-14 22:31:15 -0300

    trabajando en busqueda de pelis

--- js/jsmovies.js
+++ js/jsmovies.js
@@ -1,6 +1,7 @@
 (async function load() {
     //Constante response espera a que termine el fetch
 
+    const BASE_API = "https://yts.am/api/v2/";
 
     async function getData(url) {
         const response = await fetch(url);
@@ -14,17 +15,41 @@
 
     $hideModal.addEventListener('click', hideModal);
 
-    $form.addEventListener("submit", (event) => {
+    $form.addEventListener("submit", async (event) => {
         event.preventDefault();
         $home.classList.add('search-active');
-        showModal();
+        //Creamos loader
+        const $loader = document.createElement('img');
+        setAttributesForElement($loader, {
+            src: './img/loader.gif',
+            heigth: 50,
+            width: 50,
+        });
+        const $featuringContainer = document.getElementById('featuring');
+        $featuringContainer.append($loader);
+        const data = new FormData($form);
+        const movieSearched = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`);
+        let HTMLString;
+        if (movieSearched.data.movies == undefined) {
+            HTMLString = `
+                <div>
+                    <h3> No hay resultados </h3>
+                </div>
+            `;
+        } else {
+            HTMLString = searchedMovieTemplate(movieSearched.data.movies[0]);
+        }
+        //inyectamos en HTML
+        $featuringContainer.innerHTML = HTMLString;
+
+        // showModal();
     });
 
-    const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action');
+    const actionList = await getData(`${BASE_API}list_movies.json?genre=action`);
 
-    const terrorList = await getData('https://yts.am/api/v2/list_movies.json?genre=terror');
+    const terrorList = await getData(`${BASE_API}list_movies.json?genre=terror`);
 
-    const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation');
+    const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`);
 
     const $actionContainer = document.querySelector('#action');
     const $terrorContainer = document.getElementById('terror');
@@ -54,7 +79,19 @@ function videoItemTemplate(movie) {
         </div>
         `)
 }
-
+function searchedMovieTemplate(movie) {
+    return (
+        `<div class="featuring">
+            <div class="featuring-image">
+                <img src="${movie.medium_cover_image}" width="70" height="100" alt="${movie.title}">
+            </div>
+            <div class="featuring-content">
+                <p class="featuring-title">Pelicula encontrada</p>
+                <p class="featuring-album">${movie.title}</p>
+            </div>
+        </div>`
+    )
+}
 function createTemplate(HTMLString) {
     const html = document.implementation.createHTMLDocument();
     html.body.innerHTML = HTMLString;
@@ -104,4 +141,13 @@ function hideModal() {
 
     $overlay.classList.remove('active');
     $modal.style.animation = 'modalOut .8s forwards';
+}
+
+function setAttributesForElement($element, attributes) {
+
+    for (const attribute in attributes) {
+        //buscamos todos los valores
+        $element.setAttribute(attribute, attributes[attribute]);
+
+    }
 }
